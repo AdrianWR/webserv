00880f68f75ccf7fc73ce3aded2908556d3fb835
Adrian Roque <adrian.w.roque@gmail.com>
2022-09-12 20:29:15 -0300

    Add http handler

---
+++ lib/http/http_handler.cpp
@@ -0,0 +1,19 @@
+#include "http.hpp"
+#include "log.hpp"
+#include "utils.hpp"
+#include <exception>
+
+HttpResponse HttpHandler::generateResponse(HttpRequest &h, Config &config) {
+  HttpResponse k;
+
+  BaseHttp::HeaderMap headers = h.getHeaders();
+  Config::BlockMap blocks = config.getBlockMap();
+
+  try {
+    ConfigBlock block = blocks.at(headers["host"]);
+  } catch (std::ou) {
+    throw BaseHttp::HttpException("Invalid host configuration");
+    LOG(DEBUG) << "Invalid host configuration";
+  }
+  return k;
+}
