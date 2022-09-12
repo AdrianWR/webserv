4548e342b2d6adaeb39d62c7bee19bcb71d034e3
Paulo Cunha <pcunha@student.42sp.org.br>
2022-09-12 17:54:12 -0300

    Implement autoindex generator. Change config outouts filenames

--- lib/utils/utils.cpp
+++ lib/utils/utils.cpp
@@ -326,3 +326,58 @@ void ConfigBlock::print_block_file(std::ofstream &cout) {
   };
   cout << "=======================================================\n";
 }
+
+AutoIndexGenerator::AutoIndexGenerator(void) {
+    return ;
+}
+
+AutoIndexGenerator::AutoIndexGenerator(AutoIndexGenerator const &src) {
+    (void)src;
+    return ;
+}
+
+AutoIndexGenerator::~AutoIndexGenerator(void) {
+    return ;
+}
+
+AutoIndexGenerator	&AutoIndexGenerator::operator=(AutoIndexGenerator const &src) {
+    (void)src;
+    return *this;
+}
+
+std::string         AutoIndexGenerator::getPage(const char *path, std::string const &host, int port) {
+    std::string dirName(path);
+    DIR *dir = opendir(path);
+    std::string page =\
+    "<!DOCTYPE html>\n\
+    <html>\n\
+    <head>\n\
+            <title>" + dirName + "</title>\n\
+    </head>\n\
+    <body>\n\
+    <h1>INDEX</h1>\n\
+    <p>\n";
+
+    if (dir == NULL) {
+        std::cerr << "Error: could not open [" << path << "]" << std::endl;
+        return "";
+    }
+    if (dirName[0] != '/')
+        dirName = "/" + dirName;
+    for (struct dirent *dirEntry = readdir(dir); dirEntry; dirEntry = readdir(dir)) {
+        page += AutoIndexGenerator::getLink(std::string(dirEntry->d_name), dirName, host, port);
+    }
+    page +="\
+    </p>\n\
+    </body>\n\
+    </html>\n";
+    closedir(dir);
+    return page;
+}
+
+std::string         AutoIndexGenerator::getLink(std::string const &dirEntry, std::string const &dirName, std::string const &host, int port) {
+    std::stringstream   ss;
+    ss << "\t\t<p><a href=\"http://" + host + ":" <<\
+        port << dirName + "/" + dirEntry + "\">" + dirEntry + "</a></p>\n";
+    return ss.str();
+}
