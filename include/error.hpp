19f21232c41ff31214086d672591eb2c88100614
Paulo Cunha <pcunha@student.42sp.org.br>
2022-09-23 18:42:47 -0300

    refactors before fork

---
+++ include/error.hpp
@@ -0,0 +1,37 @@
+#ifndef ERROR_HTTP
+#define ERROR_HTTP
+
+#include "utils.hpp"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "log.hpp"
+// *****************************************************
+// Class Error
+// *****************************************************
+class Error {
+public:
+	Error();
+	Error(size_t c, ConfigBlock sc);
+	~Error();
+	Error &operator=(const Error &s);
+
+public:
+	size_t			code;
+	std::string		msg;
+	std::string		body;
+
+private:
+	std::map<size_t, std::string>	error_dic;
+
+public:
+	std::string fetch_error_page(size_t code, ConfigBlock sc);
+	void print_error();
+};
+
+#endif
---
+++ include/http_response.hpp
@@ -0,0 +1,34 @@
+#ifndef HTTPRESPONSE_HPP
+#define HTTPRESPONSE_HPP
+
+#include <map>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "utils.hpp"
+
+class HttpResponse {
+
+	typedef std::map<std::string, std::string>	HeaderMap;
+
+public:
+	HttpResponse ();
+	HttpResponse (size_t code, std::string reason, std::string body);
+	~HttpResponse ();
+	HttpResponse &operator=(const HttpResponse &s);
+
+// Atributes
+public:
+	std::string		_delimiter;
+	std::string		_version;
+	size_t			_code;
+	std::string		_reason;
+	std::string		_status_line;
+	HeaderMap		_headers;
+	std::string		_body;
+
+// Methods
+	void set(size_t code, std::string reason, std::string body);
+	std::string serialize();
+};
+#endif
