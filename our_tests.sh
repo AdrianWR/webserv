2962cbfd8ab6eb7ed3a413aaeb15918a8f0a9e35
= <=>
2022-10-17 18:28:34 -0300

    Implement variable PORTA1 in our_tests.sh

--- our_tests.sh
+++ our_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+PORTA1=8081
+
 echo "Choose test: "
 echo "1  GET: metodo permitido"
 echo "2  GET: tem redirection"
@@ -27,35 +29,35 @@ read n
 # ===========================================================
 if [ $n == 1 ]; then
 # "1  GET: metodo permitido"
-	curl -v www.site1.com:8081/getok/site1.html
+	curl -v www.site1.com:$PORTA1/getok/site1.html
 fi
 if [ $n == 2 ]; then
 # "2  GET: tem redirection"
-	curl -v www.site1.com:8081/redirection/
+	curl -v www.site1.com:$PORTA1/redirection/
 fi
 if [ $n == 3 ]; then
 # "3  GET: arquivo existe"
-	curl -v www.site1.com:8081/getok/site1.html
+	curl -v www.site1.com:$PORTA1/getok/site1.html
 fi
 if [ $n == 4 ]; then
 # "4  GET: arquivo nao existe"
-	curl -v www.site1.com:8081/getok/arquivonaoexiste.html
+	curl -v www.site1.com:$PORTA1/getok/arquivonaoexiste.html
 fi
 if [ $n == 5 ]; then
 # "5  GET: diretorio com index"
-	curl -v www.site1.com:8081/diretorio/index.html
+	curl -v www.site1.com:$PORTA1/diretorio/index.html
 fi
 if [ $n == 6 ]; then
 # "6  GET: diretorio sem index com autoindex"
-	curl -v www.site1.com:8081/outrodir/
+	curl -v www.site1.com:$PORTA1/outrodir/
 fi
 if [ $n == 7 ]; then
 #"7  GET: diretorio sem index e sem autoindex"
-	curl -v www.site1.com:8081/getok/
+	curl -v www.site1.com:$PORTA1/getok/
 fi
 if [ $n == 8 ]; then
 #8 GET: CGI (executa arquivo.py)"
-	curl -v www.site1.com:8081/cgi/get_cgi.py
+	curl -v www.site1.com:$PORTA1/cgi/get_cgi.py
 fi
 
 # ===========================================================
@@ -106,41 +108,4 @@ if [ $n == 17 ]; then
 # "17 DELETE: Arquivo existe mas nao tem permissao"
 	curl -v -X DELETE www.site3.com:8083/sempermissao/arq1
 fi
-
-
-
-# SEGUNDO TESTE DA INTRA:
-
-#POST / HTTP/1.1
-#Host: localhost:5555
-#User-Agent: Go-http-client/1.1
-#Transfer-Encoding: chunked
-#Content-Type: test/file
-#Accept-Encoding: gzip
-if [ $n == 20 ]; then
-	curl -v -H "Host: localhost:5555" -H "User-Agent: Go-http-client/1.1" -H "Transfer-Encoding: chunked" -H "Content-Type: test/file" -H "Accept-Encoding: gzip" -X POST http://localhost:8081
-fi
-
-# 1o teste 2GET: 200
-
-# 2o teste POST chunked: 405
-# 3o teste HEAD: 405
-
-# 4o teste GET/directory: 200
-# 5o teste GET/directory/youpi.bad_extension: 200
-# 6o teste GET/directory/youpi.bla: 200
-
-# 7o teste GET/directory.oulalala: 404
-
-# 8o teste GET/direcory/nop: 200
-# 9o teste GET/directory/nop/:200
-# 10o teste GET/directory/nop/other.pouic: 200
-
-# 11o teste GET/directory/nop/other.pouac: 404
-# 12o teste GET/directory/Yeah: 404
-
-# 13o teste GET/directory/Yeah/not_happy.bad_extension: 200
-# 14o teste PUT_test/file_should_exist_after: 
-
-
 
